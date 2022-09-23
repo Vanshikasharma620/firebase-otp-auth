b9c158be0a4718adcc9bf5a98f36b752cc725f24
Vanshikasharma620 <sharmavanshika620@gmail.com>
2022-09-23 15:30:40 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,39 @@
+	// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+	const firebaseConfig = {
+        apiKey: "",
+        authDomain: "",
+        projectId: "",
+        storageBucket: "",
+        messagingSenderId: "",
+        appId: "",
+        measurementId: ""
+      };
+          firebase.initializeApp(firebaseConfig);
+      render();
+      function render(){
+          window.recaptchaVerifier = new firebase.auth.RecaptchaVerifier('recaptcha-container');
+          recaptchaVerifier.render();
+      }
+          // function for send message
+      function phoneAuth(){
+          var number = document.getElementById('number').value;
+          firebase.auth().signInWithPhoneNumber(number, window.recaptchaVerifier).then(function(confirmationResult){
+              window.confirmationResult = confirmationResult;
+              coderesult = confirmationResult;
+              document.getElementById('sender').style.display = 'none';
+              document.getElementById('verifier').style.display = 'block';
+          }).catch(function(error){
+              alert(error.message);
+          });
+      }
+          // function for code verify
+      function codeverify(){
+          var code = document.getElementById('verificationcode').value;
+          coderesult.confirm(code).then(function(){
+              document.getElementsByClassName('p-conf')[0].style.display = 'block';
+              document.getElementsByClassName('n-conf')[0].style.display = 'none';
+          }).catch(function(){
+              document.getElementsByClassName('p-conf')[0].style.display = 'none';
+              document.getElementsByClassName('n-conf')[0].style.display = 'block';
+          })
+      }
